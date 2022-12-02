c0453cb59cd2f88733a04b4adf0872632acef278
dementor <dementor@gmail.com>
2022-12-02 23:28:13 +0900

    Mybatis 연결

--- src/main/java/com/myboard/controller/BoardController.java
+++ src/main/java/com/myboard/controller/BoardController.java
@@ -1,13 +1,24 @@
 package com.myboard.controller;
 
+import com.myboard.entity.Board;
+import com.myboard.mapper.BoardMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class BoardController {
 
+    @Autowired
+    private BoardMapper boardMapper;
+
     @RequestMapping("/")
-    public String main(){
+    public String main(Model model){
+        List<Board> boardList = boardMapper.getLists();
+        model.addAttribute("list", boardList);
         return "main";
     }
 }
