73dc945bdae2d202d051cbda42979bc9580f6841
dementor <dementor@gmail.com>
2022-12-03 19:09:59 +0900

    1. 리스트 조회 2. 글쓰기 화면 이동

--- src/main/java/com/myboard/controller/BoardController.java
+++ src/main/java/com/myboard/controller/BoardController.java
@@ -5,7 +5,10 @@ import com.myboard.mapper.BoardMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -21,4 +24,10 @@ public class BoardController {
         model.addAttribute("list", boardList);
         return "main";
     }
+
+    @GetMapping(value = "/boardForm.do")
+    public String boardForm(){
+        return "boardForm";
+    }
+
 }
