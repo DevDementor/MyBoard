194c7b9f998bdeabb3989e5309e51118972e7479
dementor <dementor@gmail.com>
2022-12-07 12:40:24 +0900

    init

--- src/main/java/com/myboard/controller/BoardController.java
+++ src/main/java/com/myboard/controller/BoardController.java
@@ -1,13 +1,10 @@
 package com.myboard.controller;
 
-import com.myboard.entity.Board;
 import com.myboard.mapper.BoardMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BoardController {
@@ -19,43 +16,5 @@ public class BoardController {
     public String main(Model model){
         return "main";
     }
-    @GetMapping("/boardList.do")
-    public @ResponseBody List<Board> boardList (Model model){
-        List<Board> boardList = boardMapper.getLists();
-        model.addAttribute("list", boardList);
 
-        return boardList;
-    }
-
-    @PostMapping("/boardInsert.do")
-    public @ResponseBody void boardInsert(Board board){
-        boardMapper.boardInsert(board);
-//        return "redirect:/boardList.do";
-    }
-
-    @GetMapping("/boardDelete.do")
-    public @ResponseBody void boardDelete(@RequestParam("idx")int idx){
-        boardMapper.boardDelete(idx);
-    }
-
-    @PostMapping("/boardUpdate.do")
-    @ResponseBody
-    public void boardUpdate(Board vo){
-        boardMapper.boardUpdate(vo);
-    }
-
-    @GetMapping("/boardContent.do")
-    @ResponseBody
-    public Board boardContent(int idx){
-        Board board = boardMapper.boardContent(idx);
-        return board;
-    }
-
-    @GetMapping("/boardCount.do")
-    @ResponseBody
-    public Board boardCount(int idx){
-        boardMapper.boardCount(idx);
-        Board board = boardMapper.boardContent(idx);
-        return board;
-    }
 }
